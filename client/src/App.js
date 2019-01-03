8c8cbea3d02e452e0b38710f9df74dff17c5c7e1
Teya Urena <teyaleoni@gmail.com>
2019-01-02 22:48:27 -0800

    final commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import axios from 'axios'
+import 'font-awesome/css/font-awesome.css'
 
 class App extends Component {
+  state = {
+    form: []
+  }
+
   componentDidMount() {
     axios.get('http://localhost:3001/fields').then(resp => {
-      console.log(resp.data)
+      this.setState({
+        form: resp.data
+      })
     })
   }
 
+
+  
+
   render() {
+    let fieldResult = []
+    this.state.form.forEach(field => {
+      if (field.type === "text") {
+        fieldResult.push(
+        <div key = {field.id}>
+          <i className = {"fa " + field.icon}/> <input type = {field.type} placeholder={field.label}/>
+        </div>
+        )
+        
+  
+      } else if (field.type === "email") {
+        fieldResult.push(
+          <div key = {field.id}>
+          <i className = {"fa " + field.icon} /><input type = {field.type} placeholder={field.label}/>
+        </div>)
+
+
+      } else if (field.type === "select"){
+        fieldResult.push(
+            <select key={field.id}>
+              <option value= "lang">Select Language</option>
+              {field.options.map((opt, i) => {
+                return <option key={'opt' + i} value={opt.value}>{opt.label}</option>
+              })}
+            </select>
+          
+
+      )} else if (field.type === "textarea") {
+        fieldResult.push( 
+        <div key = {field.id}>
+          <i className = {"fa " + field.icon} /><input type = {field.type} placeholder={field.label}/>
+        </div>)
+
+      } else if (field.type === "tel") {
+        fieldResult.push(
+        <div key = {field.id}>
+          <i className = {"fa " + field.icon} /><input type = {field.type} placeholder={field.label}/>
+        </div>
+        )
+      }
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Sign Up For My Web App!</h1>
+        {fieldResult}
+        <button>Submit</button>
       </div>
-    );
+      
+    )
+    
   }
 }
 
